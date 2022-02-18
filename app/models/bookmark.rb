5995f2935cde89f0011da0e9494f238112816645
Winston Chu <r.winstonc@gmail.com>
2022-02-18 11:55:21 +0900

    Added root route

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates :comment, presence:true, uniqueness: true, length: { minimum: 6 }
+  validates :movie, uniqueness: { scope: [:list] }
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,12 +1,12 @@
 class Movie < ApplicationRecord
   #assoc
   has_many :bookmarks
-  before_destroy :check_if_bookmarks
+  # before_destroy :check_if_bookmarks
 
   #valid
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
-  validates :overview, presence: true
-  validates :overview, presence: true
+  validates :poster_url, presence: true
+  validates :rating, presence: true
 
 end
